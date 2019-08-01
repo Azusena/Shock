1f84c60aad96008f281c4e1ac49f174e42cf881b
Dylan Erik Staud <dylan.staud@sjsu.edu>
2019-08-01 10:24:39 -0700

    edit

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import jinja2
 import os
 import webapp2
 
-
-class EnterInfoHandler(webapp2.RequestHandler):
-    def get(self):  # for a get request
-        welcome_template = the_jinja_env.get_template('templates/welcome.html')
-        self.response.write(welcome_template.render())
 the_jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
+class EnterInfoHandler(webapp2.RequestHandler):
+    def get(self):  # for a get request
+        welcome_template = the_jinja_env.get_template('templates/welcome.html')
+        self.response.write(welcome_template.render())
+
 app = webapp2.WSGIApplication([
-  ('/', HandleHome),
+  ('/', EnterInfoHandler),
 ], debug=True)
